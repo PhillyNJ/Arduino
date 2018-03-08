38f8a96af020fd2a21d7fe3497aff11f8833d5d9
PhillyNJ <PhillyNJ@users.noreply.github.com>
2018-03-08 06:30:41 -0500

    Update nrf24.h

--- nRF24L01/nrf24.h
+++ nRF24L01/nrf24.h
@@ -1,10 +1,4 @@
 /*
-* ----------------------------------------------------------------------------
-* “THE COFFEEWARE LICENSE” (Revision 1):
-* <ihsan@kehribar.me> wrote this file. As long as you retain this notice you
-* can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a coffee in return.
-* -----------------------------------------------------------------------------
 * This library is based on this library: 
 *   https://github.com/aaronds/arduino-nrf24l01
 * Which is based on this library: 
